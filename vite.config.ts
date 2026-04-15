4f06a2f799cbd97213f5c6de843690de65449d73
Aleksandra Mętrak <aleksandra.metrak@closbrothers.pl>
2026-04-15 15:32:02 +0200

    init vite library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'src/lib/main.ts'),
+      formats: ['es'],
+    }
+  }
 })

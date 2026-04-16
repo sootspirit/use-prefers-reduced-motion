3bf4a40a8ffe51b9c20ac586f44b3619a838c7eb
Aleksandra Mętrak <aleksandra.metrak@closbrothers.pl>
2026-04-16 09:47:42 +0200

    building types with unplugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+import dts from "unplugin-dts/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: ["src/lib"],
+      tsconfigPath: "./tsconfig.lib.json",
+      insertTypesEntry: true,
+      copyDtsFiles: true,
+    }),
+  ],
   build: {
     copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, 'src/lib/main.ts'),
-      formats: ['es'],
-    }
-  }
-})
+      entry: resolve(__dirname, "src/lib/main.ts"),
+      formats: ["es"],
+    },
+  },
+});

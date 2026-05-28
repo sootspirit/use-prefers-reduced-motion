362036802b53dd1d38b708fb6b8ef77537ba0c34
Aleksandra Mętrak <aleksandra.metrak@closbrothers.pl>
2026-05-28 13:11:42 +0200

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve, extname, relative } from 'node:path';
-
 import { fileURLToPath } from 'node:url';
 import dts from 'unplugin-dts/vite';
 import { globSync } from 'glob';
